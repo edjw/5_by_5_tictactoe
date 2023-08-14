c33f15a0c175016e70ca4d2ee48959a333bd5aff
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-08-14 16:13:57 +0100

    refresh state on navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,17 @@
 	import "../app.postcss";
 
 	import { page } from "$app/stores";
+	import { gridArray, playerScores, turn } from "$lib/store/store";
+
+
+	// Reset states when change game type
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		gridArray.reset();
+		playerScores.reset();
+		turn.set("X");
+	});
 
 	// import { io } from "$lib/webSocketConnection.js";
 	// import { onMount } from "svelte";
@@ -35,9 +46,16 @@
 
 <div class="flex flex-col items-center justify-center py-8">
 	<nav class="flex space-x-4 mb-8">
-		<a href="/" class={$page.route.id === "/" ? "outline px-4 py-2" : "px-4 py-2"}>Original</a>
+		<a
+			href="/"
+			data-sveltekit-reload
+			class={$page.route.id === "/" ? "outline px-4 py-2" : "px-4 py-2"}>Original</a
+		>
 
-		<a href="/135" class={$page.route.id === "/135" ? "outline px-4 py-2" : " px-4 py-2"}>1, 3, 5</a
+		<a
+			href="/135"
+			data-sveltekit-reload
+			class={$page.route.id === "/135" ? "outline px-4 py-2" : " px-4 py-2"}>1, 3, 5</a
 		>
 	</nav>
 	<slot />
