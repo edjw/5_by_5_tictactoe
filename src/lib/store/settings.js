a4783c37114eca0b031ab83b23aec8a5e309d412
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-08-18 23:23:58 +0100

    save the grid setting to localstorage

--- src/lib/store/settings.js
+++ src/lib/store/settings.js
@@ -1,3 +1,4 @@
 import { writable } from 'svelte/store';
+import { persist, createLocalStorage } from "@macfja/svelte-persistent-store"
 
-export const showGridLettersNumbers = writable(false);
+export const showGridLettersNumbers = persist(writable(false), createLocalStorage(), "showGridLettersNumbers")
