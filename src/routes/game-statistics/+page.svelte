6746180780e4d7b498f378e25a72fc3ad56af40c
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2023-08-20 09:42:16 +0100

    fix copy bugs

--- src/routes/game-statistics/+page.svelte
+++ src/routes/game-statistics/+page.svelte
@@ -25,25 +25,23 @@
 						In the {$totalGamesForAll[gameTitle]}
 						{$totalGamesForAll[gameTitle] === 1 ? "game" : "games"} played so far,
 						{#if stats.averageScoreDifference.leader === "X"}
-							{#if $totalGamesForAll[gameTitle] === 1}
-								X beat O by 1 point. Play more games to create an average.
-							{:else}
-								X beats O by an average of {Math.round(stats.averageScoreDifference.difference)}
-								{Math.round(stats.averageScoreDifference.difference) === 1 ? "point" : "points"}.
-							{/if}
+							X {$totalGamesForAll[gameTitle] === 1 ? "beat" : "beats"} O by an average of {Math.round(
+								stats.averageScoreDifference.difference
+							)}
+							{Math.round(stats.averageScoreDifference.difference) === 1 ? "point" : "points"}
+							{$totalGamesForAll[gameTitle] === 1 ? ". Play more games to create an average." : "."}
 						{:else if stats.averageScoreDifference.leader === "O"}
-							{#if $totalGamesForAll[gameTitle] === 1}
-								O beat X by 1 point. Play more games to create an average.
-							{:else}
-								O beats X by an average of {Math.round(stats.averageScoreDifference.difference)}
-								{Math.round(stats.averageScoreDifference.difference) === 1 ? "point" : "points"}.
-							{/if}
-						{:else if $totalGamesForAll[gameTitle] === 1}
-							X and O drew. Play more games to create an average.
+							O {$totalGamesForAll[gameTitle] === 1 ? "beat" : "beats"} X by an average of {Math.round(
+								stats.averageScoreDifference.difference
+							)}
+							{Math.round(stats.averageScoreDifference.difference) === 1 ? "point" : "points"}
+							{$totalGamesForAll[gameTitle] === 1 ? ". Play more games to create an average." : "."}
 						{:else}
-							On average, X and O draw.
+							{$totalGamesForAll[gameTitle] === 1
+								? "X and O drew. Play more games to create an average."
+								: "On average, X and O draw."}
 						{/if}
-					</p>Ì
+					</p>
 
 					{#if !isNaN(stats.xWinPercentage) && !isNaN(stats.oWinPercentage) && !isNaN(stats.drawPercentage)}
 						<ul class="mt-4">
